fb3c84b7a7a24c1b4dd0b9c3396889e89a79e9df
Ankit Anand <76127674+Ankit-Anand-01@users.noreply.github.com>
2020-12-22 20:27:07 +0530

    Add files via upload

---
+++ float.go
@@ -0,0 +1,16 @@
+//use of floating-point numbers
+package main
+
+import "fmt"
+
+func main() {
+	//declaration of floating-point numbers
+	x := 50.55
+	y := 25.25
+	//addition of two floating-point numbers
+	z := x + y
+	fmt.Printf("sum = %f", z)
+	//display the type of z variable
+	fmt.Printf("\n z is a %T", z)
+
+}

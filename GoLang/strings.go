97299df7f637f779418b6afdb44bbaed2a5b4d2a
Ankit Anand <76127674+Ankit-Anand-01@users.noreply.github.com>
2020-12-23 20:20:19 +0530

    Add files via upload

---
+++ GoLang/strings.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	//declaration of a string variable
+	str := "AnkitAnand"
+	// Display the length of the string
+	fmt.Printf("Length of the string is %d", len(str))
+
+	// Display the string
+	fmt.Printf("\nString is %s", str)
+
+	// Display the type of str variable
+	fmt.Printf("\nType of str is %T", str)
+}

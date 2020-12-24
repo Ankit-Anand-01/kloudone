9bcb75b9f2cbcd157dd43f8b80ef769a1a463168
Ankit Anand <76127674+Ankit-Anand-01@users.noreply.github.com>
2020-12-24 20:02:40 +0530

    Add files via upload

---
+++ GoLang/if.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 500
+	//using the if statement to check the condition
+	if x < 1000 {
+		fmt.Printf("x is less than 1000\n")
+	}
+	fmt.Printf("value of x is %d\n", x)
+}

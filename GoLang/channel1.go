18be532ad1ad7ed3dc1f27a54d9bfec541add0bc
Ankit Anand <76127674+Ankit-Anand-01@users.noreply.github.com>
2021-03-22 17:49:32 +0530

    Update channel1.go

--- GoLang/channel1.go
+++ GoLang/channel1.go
@@ -1,19 +1,21 @@
+//Channels in Golang
 package main
 
 import (
 	"fmt"
 	"time"
 )
-
+//defining func to print
 func display(ch chan string) {
 	fmt.Println(<-ch)
 	fmt.Println(<-ch)
 }
 func main() {
+	//initializing the channel 
 	ch := make(chan string)
-
+       //calling goroutine
 	go display(ch)
-
+       //sending values to the channels
 	ch <- "Ping"
 	ch <- "Pong"
 	time.Sleep(100 * time.Millisecond)

8b185d6dc859c90009a0b5463bdfafea373291b3
Ankit Anand <76127674+Ankit-Anand-01@users.noreply.github.com>
2021-03-30 13:24:09 +0530

    Update remove.go

--- GoLang/remove.go
+++ GoLang/remove.go
@@ -1,17 +1,22 @@
+//remove duplicates using nested loops in Golang
 package main
 
 import "fmt"
 
-func rd(a []int) []int {
+func removeDuplicates(a []int) []int {
 	result := []int{}
 	n := len(a)
+	//traversing array
 	for i := 0; i < n; i++ {
 		j := 0
+		//checking the element has come before or not
 		for ; j < i; j++ {
+			//if element has come before
 			if a[i] == a[j] {
 				break
 			}
 		}
+		//element has not come before
 		if j == i {
 			result = append(result, a[i])
 
@@ -22,5 +27,5 @@ func rd(a []int) []int {
 }
 func main() {
 	s := []int{5, 6, 3, 2, 1, 4, 5, 6, 3, 2, 1}
-	fmt.Println(rd(s))
+	fmt.Println(removeDuplicates(s))
 }

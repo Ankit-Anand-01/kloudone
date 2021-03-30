6484f769d6d57a2156db4df337da80d5f0419f11
Ankit Anand <76127674+Ankit-Anand-01@users.noreply.github.com>
2021-03-30 10:41:13 +0530

    Add files via upload

---
+++ GoLang/upper.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	s := "ankit"
+	s2 := "ANAND"
+	//print in uppercase
+	fmt.Println(strings.ToUpper(s))
+	fmt.Println(strings.ToLower(s2))
+}
